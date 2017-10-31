72cb6f3bbde971b12615dff69b8e3a4caf447cb2
AbdulRazaak <abdulrazaak4480@gmail.com>
2017-10-31 21:57:15 +0630

    Add a database connection

---
+++ database.php
@@ -0,0 +1,16 @@
+<?php
+//Connect to MySQL
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$database = 'jsshoutbox'
+
+$con = mysqli_connect($host,$user,$password,$database);
+
+//Checking a connection
+
+if (mysqli_connect_errno()){
+    echo 'Failed to connect: ' .mysqli_connect_error();
+}
+?>
+
